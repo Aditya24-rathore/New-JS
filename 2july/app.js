9628de7bb616e0fee610a19167155b756d480c9b
Adityaraj Rathore <ra4402538@gmail.com>
2025-07-03 07:10:58 -0700

    update

--- 2july/app.js
+++ 2july/app.js
@@ -26,11 +26,46 @@
 // }
 
 // WAP to take two number from user and check num2 is even or odd if num2 is even print num2 and num2 is odd else multiply num1 and num2
-let num1=parseInt(prompt("enter first number"))
-let num2=parseInt(prompt("enter second number"))
-if(num2%2==0){
-    console.log(num2)
+// let num1=parseInt(prompt("enter first number"))
+// let num2=parseInt(prompt("enter second number"))
+// if(num2%2==0){
+//     console.log(num2)
+// }
+// else{
+//     console.log(num1*num2)
+// }
+
+// WAP to find the greatest of two number 
+// let num1=parseInt(prompt("enter num1"))
+// let num2=parseInt(prompt("enter num2"))
+// if(num1>num2){
+//     console.log(num1,"num1 is greater")
+// }
+// else if(num1==num2){
+//     console.log("Both are equal")
+// }
+// else{
+//      console.log(num2,"num2 is greater")
+// }
+
+// WAP to check valid age
+// let age=parseInt(prompt("enter ur age"))
+// if(age>0 && age<=100){
+//   if(age>=18){
+//     alert("you can vote")
+//   }
+//   else{
+//     alert("you are not eligible")
+//   }
+// }
+// else{
+//     alert("please enter valid age")
+// }
+
+let char=prompt("enter character")
+if(char=='a'|| char=='e' || char=='i' || char=='o' || char=='u' ||char=='A'|| char=='E' || char=='I' || char=='O' || char=='U'){
+    alert("char is vowel")
 }
 else{
-    console.log(num1*num2)
+    alert("char is constant")
 }
---
+++ 3july/app.js
@@ -0,0 +1,50 @@
+// Ladder conditional statement
+// let num1=parseInt(prompt("enter number"))
+// let num2=parseInt(prompt("enter number"))
+// let num3=parseInt(prompt("enter number"))
+// let total=num1+num2+num3
+// let result=(total/300)*100
+// if(result>=75){
+//     console.log("pass with grade A")
+// }
+// else if(result>=60 && result<75){
+//     console.log("pass with grade B")
+// }
+// else if(result>=33 && result<60){
+//     console.log("pass with grade C")
+// }
+// else{
+//     console.log("fail")
+// }
+
+// Nested if-else
+// let age=parseInt(prompt("enter age"))
+// if(age>=18){
+//     let age2=parseInt(prompt("press 1 for indian"))
+//     if(age2==1){
+//         console.log("ur eligible")
+//     }
+//     else{
+//         console.log("ur are not eligible")
+//     }
+// }
+// else{
+//     console.log("minor")
+// }
+
+
+// Do while
+// let num1=1
+// do{
+//     console.log(num1)
+//     num1++
+// }while(num1<=10)
+
+let num1=10
+let n=parseInt(prompt("enter number"))
+do{
+    console.log(num1*n)
+    num1--
+}while(num1>=1)
+
+
