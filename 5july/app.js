b2afdf17a33d50d402b9f5d34e41fe59402356d5
Adityaraj Rathore <ra4402538@gmail.com>
2025-07-05 08:37:03 -0700

    update

---
+++ 5july/app.js
@@ -0,0 +1,17 @@
+// let arr=["cybrom",18,'M']
+// console.log(arr[2])
+
+// let ar1=[]
+// for(a=0;a<5;a++){
+//    ar1[a]=prompt("enter elements")
+// }
+// console.log(ar1)
+
+let ar2=[1,2,3,4,5,6,7,8,9,10]
+let sum=0
+for(a=0;a<10;a++){
+   if(ar2[a]%2==0){
+    sum=sum+ar2[a]
+   }
+}
+console.log(sum)
