f1782563873a8c0a466e47c685577e806d804dc4
Adityaraj Rathore <ra4402538@gmail.com>
2025-07-13 04:19:50 -0700

    update

---
+++ 12july string/app.js
@@ -0,0 +1,17 @@
+// Replace Method
+let str="This is Bhopal which is best in bhopal "
+console.log(str.replace(/Bhopal/gi,"MP"))
+
+
+// Split method: it is used to convert string into array
+let text="Apple Mango Banana"
+console.log(text.split(" "))
+
+// Char Code AT
+let text1="Heloo from js"
+console.log(text1.charCodeAt())
+ 
+
+// CharAt
+let text2="hello from js"
+console.log(text2.charAt(0));
