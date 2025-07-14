7a21a1328edbf58e9532e53863f744d60ee8b363
Adityaraj Rathore <ra4402538@gmail.com>
2025-07-14 07:14:06 -0700

    js

---
+++ 14july/app.js
@@ -0,0 +1,19 @@
+// Template literals
+// Support multiline string(use 1)
+let name=`Aditya
+           Raj
+           Rathore`
+console.log(name)
+
+// Support html tags (use 2)
+let name1='<h1>Aditya Raj Rathore</h1>'
+document.write(name1)
+
+// Support double and single quotes(use 3)
+let name2='Aditya "Raj" Rathore'
+console.log(name2)
+
+// (use 4)
+let name3="Aditya"
+let age=20
+console.log(`My name is ${name3} and my age ${age} is years old`);
