41953822e48df90862556a4281554a191b248d20
Adityaraj Rathore <ra4402538@gmail.com>
2025-07-15 06:32:54 -0700

    DOM js

---
+++ 15july DOM/app.js
@@ -0,0 +1,27 @@
+// DOM is a tree like strucure of html where js can change,delete dynamically
+function addText() {
+    // let T=document.getElementById("text")
+    let T=document.querySelector("#text")
+    // T.innerHTML="Hello from Js"
+    T.innerText="Hello from js"
+    T.style.color="red"
+    // T.style.backgroudColor="black"
+
+}
+
+function Change(){
+    let Y=document.querySelector("#text1")
+    let X=document.querySelector("#text2")
+    X.innerHTML="hello from Html"
+    Y.innerHTML="Text from js"
+}
+
+function changeColor(){
+    let Cr=document.querySelector("#colour")
+    Cr.style.color="Red"
+}
+
+function Red(){
+    let Cg=document.querySelector("#colour")
+    Cg.style.color="Green"
+}
