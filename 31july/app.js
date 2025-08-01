d76c81b6251baeeab472b3b72003b4cf2d6073e2
Adityaraj Rathore <ra4402538@gmail.com>
2025-08-01 07:10:15 -0700

    update

--- 31july/app.js
+++ 31july/app.js
@@ -3,17 +3,43 @@ console.log(Math.floor(Math.random()*100));
 console.log(Math.floor(1000+Math.random()*9000));
 console.log(Math.floor(1+Math.random()*6));
 
-let okk=()=>{
-    setTimeout(()=>{
-        alert("Hello")
-    },3000)
+// let okk=()=>{
+//     setTimeout(()=>{
+//         alert("Hello")
+//     },3000)
+// }
+
+// let a=0
+// let inter
+// let ok=()=>{
+//   inter= setInterval(()=>{
+        
+//         console.log(a)
+//         a++
+//     },3000)
+// }
+
+// let stopp=()=>{
+//     clearInterval(inter)
+// }
+
+var a 
+let c=0
+let ok=()=>{
+
+     a = setInterval(()=>{
+     let b = document.getElementById('display') 
+     b.innerHTML = c
+        c++
+    },
+        1000
+    )
+
+    if(c==10){
+      clearInterval(a)
+    }
 }
 
-let a=0
-let ok=()=>{
-    setInterval(()=>{
-        
-        console.log(a)
-        a++
-    },3000)
+let stopp=()=>{
+    clearInterval(a)
 }
